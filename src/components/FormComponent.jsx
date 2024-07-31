ad7b2ac6606987144820e7c3f241eb23de9659d7
Jenna Cioffi <JMCioffi4@gmail.com>
2024-07-31 16:00:03 -0600

    I guess these never pushed, pushing now

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -6,6 +6,7 @@ const FormComponent = ({ onSubmit }) => {
     name: "",
     email: "",
     message: "",
+    phonenumber: "",
   });
 
   const handleChange = (e) => {
@@ -22,46 +23,141 @@ const FormComponent = ({ onSubmit }) => {
       name: "",
       email: "",
       message: "",
+      phonenumber: "",
     });
   };
 
   return (
-    <div>
-      <h2>Submit Form</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
+    <div style={{
+        display: 'flex',
+        width: '100vw',
+        flexDirection: 'column',
+        textAlign: 'center',
+        flex: 1,
+    }}>
+      <h2 style={{
+        fontSize: 30,
+      }}>Submit Form</h2>
+      <form onSubmit={handleSubmit} style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}>
+        <label style={{
+          fontSize: 20,
+          display: 'flex',
+          alignContent: 'space-around',
+          flexDirection: 'column',
+          textAlign: 'left',
+          paddingTop: '10px',
+          paddingBottom: '10px',
+          flex: 1,
+        }}>
           Name:
           <input
             type="text"
             name="name"
             value={formData.name}
             onChange={handleChange}
+            style={{
+              borderWidth: '2px',
+              borderColor: 'black',
+              borderRadius: '10px',
+              flex: 1,
+              width: '250px',
+            }}
             required
           />
         </label>
         <br />
-        <label>
+        <label style={{
+          fontSize: 20,
+          display: 'flex',
+          alignContent: 'space-around',
+          flexDirection: 'column',
+          textAlign: 'left',
+          // paddingTop: '10px',
+          paddingBottom: '10px',
+          flex: 1,
+        }}>
           Email:
           <input
             type="email"
             name="email"
             value={formData.email}
             onChange={handleChange}
+            style={{
+              borderWidth: '2px',
+              borderColor: 'black',
+              borderRadius: '10px',
+              flex: 1,
+              width: '250px',
+            }}
             required
           />
         </label>
         <br />
-        <label>
+        <label style={{
+          fontSize: 20,
+          display: 'flex',
+          alignContent: 'space-around',
+          flexDirection: 'column',
+          textAlign: 'left',
+          // paddingTop: '10px',
+          paddingBottom: '10px',
+          flex: 1,
+        }}>
           Message:
           <textarea
             name="message"
             value={formData.message}
             onChange={handleChange}
+            style={{
+              borderWidth: '2px',
+              borderColor: 'black',
+              borderRadius: '10px',
+              flex: 1,
+              width: '250px',
+            }}
+            required
+          />
+        </label>
+        <br/>
+        <label style={{
+          fontSize: 20,
+          display: 'flex',
+          alignContent: 'space-around',
+          flexDirection: 'column',
+          textAlign: 'left',
+          // paddingTop: '10px',
+          paddingBottom: '10px',
+          flex: 1,
+        }}>
+          Phone Number:
+          <input
+            name="phonenumber"
+            type="text"
+            value={formData.phonenumber}
+            onChange={handleChange}
+            style={{
+              borderWidth: '2px',
+              borderColor: 'black',
+              borderRadius: '10px',
+              flex: 1,
+              width: '250px',
+            }}
             required
           />
         </label>
         <br />
-        <button type="submit">Submit</button>
+        <button type="submit" style={{
+          width: '150px',
+          height: '75px',
+          borderRadius: '10px',
+          borderColor: 'green',
+          borderWidth: '3px',
+          fontSize: 20
+        }}>Submit</button>
       </form>
     </div>
   );
--- src/components/TableComponent.jsx
+++ src/components/TableComponent.jsx
@@ -3,23 +3,62 @@ import React from "react";
 const TableComponent = ({ data }) => {
   return (
     <div>
-      <h2>Submitted Data Table</h2>
-      <table>
-        <thead>
+      <h2 style={{
+        display: 'flex',
+        justifyContent: 'center',
+        fontSize: 30,
+        paddingTop: '25px',
+      }}>Submitted Data Table</h2>
+      <table style={{
+        display: 'flex',
+        justifyContent: 'center',
+        borderCollapse: 'collapse',
+        borderWidth: '2px',
+        flexDirection: 'column',
+      }}>
+        <thead style={{
+          display: 'flex',
+          justifyContent: 'center',
+          borderCollapse: 'collapse',
+          borderWidth: '2px',
+          borderColor: 'red',
+        }}>
           <tr>
             <th>ID</th>
             <th>Name</th>
             <th>Email</th>
             <th>Message</th>
+            <th>Phone Number</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody style={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}>
           {data.map((item, index) => (
-            <tr key={index}>
-              <td>{item.id}</td>
-              <td>{item.name}</td>
-              <td>{item.email}</td>
-              <td>{item.message}</td>
+            <tr key={index} style={{
+              padding: '10px',
+            }}>
+              <td style={{
+                padding: '10px',
+                borderWidth: '2px',
+              }}>{item.id}</td>
+              <td style={{
+                padding: '10px',
+                borderWidth: '2px',
+              }}>{item.name}</td>
+              <td style={{
+                padding: '10px',
+                borderWidth: '2px',
+              }}>{item.email}</td>
+              <td style={{
+                padding: '10px',
+                borderWidth: '2px',
+              }}>{item.message}</td>
+              <td style={{
+                padding: '10px',
+                borderWidth: '2px',
+              }}>{item.phonenumber}</td>
             </tr>
           ))}
         </tbody>
